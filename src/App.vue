f9a2e76483ebf68330f2fca32bc152dd8d0e9e10
Артем Антонянц <aaaartemaa@gmail.com>
2025-06-18 00:37:16 +0700

    chore: login form part UI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useQuasar } from "quasar";
-const $q = useQuasar();
-const name = ref("");
+import LoginForm from "./widgets/LoginForm.vue";
 </script>
 
 <template>
-  <div>
-    <h1>ПРИВЕТ</h1>
-    <q-input v-model="name" label="Введите имя" filled />
-
-    <q-btn label="Показать" color="primary" class="q-mt-md" @click="$q.notify(`Привет, ${name || 'незнакомец'}!`)" />
-  </div>
+  <LoginForm />
 </template>
 
 <style scoped></style>
---
+++ src/widgets/LoginForm.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const mail = ref("");
+const pass = ref("");
+</script>
+
+<template>
+  <q-card class="container">
+    <q-card-section class="content">
+      <div class="headarea">
+        <q-separator />
+        <p class="text">Введите учетные данные</p>
+        <q-separator />
+      </div>
+      <div class="inputarea">
+        <q-input class="input" outlined v-model="mail" label="адрес электронной почты" />
+        <q-input class="input" outlined v-model="pass" label="пароль" />
+      </div>
+      <q-btn class="button" unelevated color="primary" label="Вход" icon-right="arrow_forward" />
+    </q-card-section>
+  </q-card>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin: auto;
+  max-width: 370px;
+  height: 290px;
+  border-radius: 15px;
+
+  .headarea {
+    display: grid;
+    grid-template-columns: 1fr max-content 1fr;
+    align-items: center;
+    gap: 5px;
+    padding-bottom: 15px;
+  }
+
+  .inputarea {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    padding-bottom: 40px;
+  }
+
+  .content {
+    padding-top: 32px;
+    height: 100%;
+  }
+
+  .text {
+    color: #a8a099;
+    font-size: 12px;
+    margin: 0;
+  }
+
+  .input {
+    ::v-deep(.q-field__control) {
+      border-radius: 10px;
+    }
+  }
+
+  .button {
+    width: 100%;
+    border-radius: 15px;
+    ::v-deep(.q-icon) {
+      margin-left: 5px;
+      font-size: 20px;
+    }
+  }
+}
+</style>
