508ad6c2ee2792669ce9f7afb4cb03bd5bf478bc
Артем Антонянц <aaaartemaa@gmail.com>
2025-06-17 23:23:55 +0700

    chore: add quasar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+const $q = useQuasar();
+const name = ref("");
+</script>
 
 <template>
-  <h1>ПРИВЕТ</h1>
+  <div>
+    <h1>ПРИВЕТ</h1>
+    <q-input v-model="name" label="Введите имя" filled />
+
+    <q-btn label="Показать" color="primary" class="q-mt-md" @click="$q.notify(`Привет, ${name || 'незнакомец'}!`)" />
+  </div>
 </template>
 
 <style scoped></style>
