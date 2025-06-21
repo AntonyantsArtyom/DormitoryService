761033095d782281126d824af8f1c859b1823fec
Артем Антонянц <aa.artem.0303@mail.ru>
2025-06-21 14:44:10 +0700

    chore: part of styles for tabs

--- src/shared/UI/TabsComponent.vue
+++ src/shared/UI/TabsComponent.vue
@@ -22,16 +22,28 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <p v-if="props?.label">{{ props.label }}</p>
+  <div class="tabscontainer">
+    <p class="title" v-if="props?.label">{{ props.label }}</p>
     <q-tabs v-model="tab" class="text-primary" dense active-color="primary" indicator-color="primary">
       <q-tab v-for="t in tabs" :key="t.name" :name="t.name" :label="t.label" />
     </q-tabs>
   </div>
 
   <q-tab-panels v-model="tab" animated>
-    <q-tab-panel v-for="t in tabs" :key="t.name" :name="t.name">
+    <q-tab-panel class="itemcontainer" v-for="t in tabs" :key="t.name" :name="t.name">
       <component :is="t.content" />
     </q-tab-panel>
   </q-tab-panels>
 </template>
+
+<style lang="scss" scoped>
+.tabscontainer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.itemcontainer {
+  padding: 0px;
+}
+</style>
