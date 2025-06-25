63b0a5696d08d026e4d215c54fbe10810c0b152c
Артем Антонянц <aa.artem.0303@mail.ru>
2025-06-25 16:12:00 +0700

    feat: getWholeUser action

--- src/shared/states/UserStore.ts
+++ src/shared/states/UserStore.ts
@@ -1,19 +1,45 @@
-// stores/user.ts
 import { defineStore } from "pinia";
 import { api } from "../../api";
 
-export type TUser = {
+export type TSession = {
   id: string;
   accessToken: string;
 };
 
+export type TUser = {};
+
 export interface IUserState {
-  user: TUser | null;
+  session: TSession | null;
+  user: IUser | null;
   loading: boolean;
+}
+
+type CertificateType = "CERTIFICATE" | "FLUOROGRAPHY";
+
+interface MedicalCertificate {
+  id: string;
+  user: string;
+  type: CertificateType;
+  startDate: string;
+  endDate: string;
+}
+
+interface IUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  roomNumber: string;
+  role: string[];
+  balance: number;
+  pediculosis: MedicalCertificate;
+  fluorography: MedicalCertificate;
+  activeEvents: any[];
 }
 
 export const useUserStore = defineStore("user", {
   state: (): IUserState => ({
+    session: null,
     user: null,
     loading: false,
   }),
@@ -22,8 +48,17 @@ export const useUserStore = defineStore("user", {
     async login(email: string, password: string) {
       this.loading = true;
       try {
-        const res = await api.post<TUser>("/sessions", { email, password });
+        const res = await api.post<TSession>("/sessions", { email, password });
         localStorage.setItem("accessToken", res.data.accessToken);
+        this.session = res.data;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async getWholeUser() {
+      this.loading = true;
+      try {
+        const res = await api.get<IUser>("api/get/whole/user");
         this.user = res.data;
       } finally {
         this.loading = false;
