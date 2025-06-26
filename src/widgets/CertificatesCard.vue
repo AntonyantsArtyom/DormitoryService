a03884b4459d958a9e13aa55d770239401cec9d9
Артем Антонянц <aa.artem.0303@mail.ru>
2025-06-26 17:47:48 +0700

    chore: render certificates info

--- src/widgets/CertificatesCard.vue
+++ src/widgets/CertificatesCard.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+import { useUserStore } from "../shared/states/UserStore";
+import dayjs from "dayjs";
+
+const userStore = useUserStore();
+
+function getTimeProgressPercent(startDate: string | undefined, endDate: string | undefined): number {
+  if (!startDate || !endDate) {
+    return 0;
+  }
+
+  const start = dayjs(startDate);
+  const end = dayjs(endDate);
+  const now = dayjs();
+
+  if (now.isAfter(end)) return 0;
+  if (now.isBefore(start)) return 0;
+
+  const total = end.diff(start, "millisecond");
+  const passed = now.diff(start, "millisecond");
+
+  return 1 - passed / total;
+}
+</script>
+
 <template>
   <q-card class="container">
     <q-card-section class="content">
@@ -5,13 +30,13 @@
       <q-separator />
       <div class="valuecontainer">
         <p class="valuetitle">Педикулез и чесотка</p>
-        <q-linear-progress class="progress" :value="0.3" />
-        <p class="date">дата окончания: 12.02.23</p>
+        <q-linear-progress class="progress" :value="getTimeProgressPercent(userStore.user?.pediculosis.startDate, userStore.user?.pediculosis.endDate)" />
+        <p class="date">дата окончания: {{ userStore.user?.pediculosis.endDate }}</p>
       </div>
       <div class="valuecontainer">
         <p class="valuetitle">Флюорография</p>
-        <q-linear-progress class="progress" :value="0.6" />
-        <p class="date">дата окончания: 12.02.23</p>
+        <q-linear-progress class="progress" :value="getTimeProgressPercent(userStore.user?.fluorography.startDate, userStore.user?.fluorography.endDate)" />
+        <p class="date">дата окончания: {{ userStore.user?.fluorography.endDate }}</p>
       </div>
     </q-card-section>
   </q-card>
